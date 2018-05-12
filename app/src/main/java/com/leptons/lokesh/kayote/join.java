c582a7bcfd013e3ef3bbd30369117e009ccd9bdc
lokesh_coder <mexican.dirtyfellow@gmail.com>
2018-05-12 23:38:03 +0530

    Initial commit

---
+++ app/src/main/java/com/leptons/lokesh/kayote/join.java
@@ -0,0 +1,13 @@
+package com.leptons.lokesh.kayote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class join extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+    }
+}
